ea4b314c01092e4655a9814e194b57acace6a16c
diogo <51544394+rareusernames@users.noreply.github.com>
2026-06-11 13:14:59 +0100

    Added new Enums to reflect new service logic.

--- helpdeskMain/eupago/domain/TransactionEvent.py
+++ helpdeskMain/eupago/domain/TransactionEvent.py
@@ -1,7 +1,20 @@
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
 
 from eupago.domain import Money
+
+class TransactionStatus(Enum):
+    PAID = "PAID"
+    REFUNDED = "REFUNDED"
+    ERROR = "ERROR"
+    CANCELED = "CANCELED"
+    EXPIRED = "EXPIRED"
+
+class PaymentMethod(Enum):
+    MBWAY = "MBWAY"
+    MBREFERENCE = "MBREFERENCE"
+    CREDITCARD = "CREDITCARD"
 
 @dataclass
 class TransactionEvent:
@@ -13,4 +26,4 @@ class TransactionEvent:
     fees: Money
     date: datetime
     trid: str
-    status: str
+    status: TransactionStatus
